d4e7bdbb5e73ce9ba64eec15d241cf7c29993fed
Anusree <anusreeprakash730@gmail.com>
2025-10-17 10:21:47 +0530

    helo

--- project/project/urls.py
+++ project/project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +29,7 @@ urlpatterns = [
     path('home',views.home,name='home'),
     path('logout',views.logout,name='logout'),
     path('main',views.mainpage,name='main'),
+
 ]
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
